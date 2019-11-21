7dafa4e28d6f73cce69ea42e3591754ea95af429
milkrong <milkrong121@outlook.com>
2019-11-20 18:34:32 -0800

    Update infopanel

--- src/components/InfoPanel.vue
+++ src/components/InfoPanel.vue
@@ -1,19 +1,38 @@
 /* eslint-disable vue/no-unused-vars */
 <template>
   <div class="info-panel">
-      <el-row v-for="title in cardTitles" :key="title.index">
+      <el-row v-for="card in cards" :key="card.index">
           <el-col :span="24">
+              <info-card :card="card"></info-card>
           </el-col>
       </el-row>
   </div>
 </template>
 
 <script>
+import infoCard from '@/components/infoCard';
+
 export default {
     name: 'info-panel',
+    components: {
+        "info-card": infoCard
+    },
     data () {
         return {
-            cardTitles: ['Time and Date', 'Monthly Spent', 'Eletrical Usage']
+            cards: [
+                {
+                    title: "Time and Date",
+                    data: "2019-10-10"
+                },
+                {
+                    title: "Monthly Spent",
+                    data: "2000"
+                },
+                {
+                    title: "Electrical Usage",
+                    data: "1.45"
+                }
+            ]
         }
     }
 }
---
+++ src/components/infoCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="info-card">
+        <el-card>
+            <div slot="header">
+                <span>{{ card.title }}</span>
+            </div>
+            {{ card.data }}
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'info-card',
+    props: ['card']
+}
+</script>
+
+<style scoped>
+
+</style>
